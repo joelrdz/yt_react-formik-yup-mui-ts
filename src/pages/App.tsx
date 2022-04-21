e9ea4e80a5ccbe30223cf153d419f69fd4cfd4a7
Joel Rdz <joelrdzh@gmail.com>
2022-04-21 14:40:34 -0500

    project config
    - main libs installation
    - main folder structure
    - global styles

---
+++ src/pages/App.tsx
@@ -0,0 +1,9 @@
+// PLUGINS IMPORTS //
+// COMPONENTS IMPORTS //
+// EXTRA IMPORTS //
+
+const App = () => {
+  return <div>React Multi-Step Form with Formik and Yup, using TypeScript</div>;
+};
+
+export default App;
