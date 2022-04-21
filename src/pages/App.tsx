15e8b6232b76a051c1d062b4f7fca4bfac1eb466
Joel Rdz <joelrdzh@gmail.com>
2022-04-21 15:05:04 -0500

    mui Stepper component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,22 @@
 // PLUGINS IMPORTS //
-import { Typography } from '@material-ui/core';
+import { Typography, Stepper, Step, StepLabel } from '@material-ui/core';
 // COMPONENTS IMPORTS //
 import { Wrapper } from 'components/layout'
 // EXTRA IMPORTS //
+
+const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
 const App = () => {
   return (
     <Wrapper>
       <Typography>Checkout</Typography>
+      <Stepper>
+        {steps.map(label => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
     </Wrapper>
   );
 };
