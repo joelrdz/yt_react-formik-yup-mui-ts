77d9735803202e65b30b442391d6c287f84430a0
Joel Rdz <joelrdzh@gmail.com>
2022-04-21 14:44:31 -0500

    mui4: theme definition

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 import 'styles/index.css';
 
+import { ThemeProvider } from '@material-ui/core';
+import { theme } from 'styles/theme.styles';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.styles.ts
@@ -0,0 +1,11 @@
+// PLUGINS IMPORT //
+import { createTheme } from '@material-ui/core/styles';
+import { cyan } from '@material-ui/core/colors';
+
+export const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: cyan,
+    secondary: cyan
+  }
+});
