63d6ea43247b2e99aa50b07ec83f7656a611d8ee
Joel Rdz <joelrdzh@gmail.com>
2023-01-17 01:33:10 -0600

    renderStepContent base functionality

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 // PLUGINS IMPORTS //
+import { useState } from 'react';
 import { Typography, Stepper, Step, StepLabel } from '@material-ui/core';
 // COMPONENTS IMPORTS //
 import { Wrapper } from 'components/layout'
@@ -7,6 +8,19 @@ import { Wrapper } from 'components/layout'
 const steps = ['Shipping address', 'Payment details', 'Review your order'];
 
 const App = () => {
+  const [activeStep, setActiveStep] = useState(1);
+
+  const renderStepContent = (step: number) => {
+    switch (step) {
+      case 0:
+        return <div>Shipping address</div>;
+      case 1:
+        return <div>Payment details</div>;
+      case 2:
+        return <div>Review your order</div>;
+    }
+  };
+
   return (
     <Wrapper>
       <Typography>Checkout</Typography>
@@ -17,6 +31,8 @@ const App = () => {
           </Step>
         ))}
       </Stepper>
+
+      { renderStepContent(activeStep) }
     </Wrapper>
   );
 };
