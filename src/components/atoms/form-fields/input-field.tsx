b4256f2a453afb833dc4ebcefe52c816ba858231
Joel Rdz <joelrdzh@gmail.com>
2023-04-25 01:33:52 -0600

    InputField: renderHelperText function
    - using lodash at function

--- src/components/atoms/form-fields/input-field.tsx
+++ src/components/atoms/form-fields/input-field.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { useField } from 'formik';
+import { at } from 'lodash';
 import { TextField, TextFieldProps } from '@material-ui/core';
 
 interface PropsType {
@@ -11,10 +12,18 @@ const InputField: FC<PropsType> = props => {
   const { errorText, ...restProps } = props;
   const [field, meta] = useField(props.defaultProps.name);
 
+  const renderHelperText = () => {
+    const [touched, error] = at(meta, 'touched', 'error');
+
+    if (touched && error) {
+      return error;
+    };
+  };
+
   return (
     <TextField
       type="text"
-      helperText={'Helper text'}
+      helperText={renderHelperText()}
       {...field}
       {...restProps.defaultProps}
     />
