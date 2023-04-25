f4fb17c766de5a1d28b6c3d8d32755f70dabda9c
Joel Rdz <joelrdzh@gmail.com>
2023-04-25 13:39:51 -0600

    dummy data for SelectInput components

--- src/components/organisms/forms/address-form.tsx
+++ src/components/organisms/forms/address-form.tsx
@@ -18,7 +18,7 @@ const AddressForm: FC<PropsType> = props => {
   } = props;
 
   return (
-    <div>
+    <>
       <Typography>Shipping address</Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
@@ -58,8 +58,65 @@ const AddressForm: FC<PropsType> = props => {
           />
         </Grid>
       </Grid>
-    </div>
+    </>
   );
 };
+
+const cities = [
+  {
+    value: undefined,
+    label: "None",
+  },
+  {
+    value: "1",
+    label: "New York",
+  },
+  {
+    value: "2",
+    label: "Chicago",
+  },
+  {
+    value: "3",
+    label: "Saigon",
+  },
+];
+
+const states = [
+  {
+    value: undefined,
+    label: "None",
+  },
+  {
+    value: "11",
+    label: "Florida",
+  },
+  {
+    value: "22",
+    label: "Michigan",
+  },
+  {
+    value: "33",
+    label: "Texas",
+  },
+];
+
+const countries = [
+  {
+    value: null,
+    label: "None",
+  },
+  {
+    value: "111",
+    label: "United States",
+  },
+  {
+    value: "222",
+    label: "Italy",
+  },
+  {
+    value: "333",
+    label: "Vietnam",
+  },
+];
 
 export default AddressForm;
